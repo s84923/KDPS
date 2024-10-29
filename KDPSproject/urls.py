705a4973816b0d7bcf6834fcd4b50c30bce1f210
ueharaharuya <oky2384858@stu.o-hara.ac.jp>
2024-10-29 05:13:03 +0000

    f

--- KDPSproject/urls.py
+++ KDPSproject/urls.py
@@ -4,5 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),         # Django 管理画面
     path('', include('KDPS.urls')),          # KDPS アプリの URL をインクルード
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # Django 認証URLをインクルード
+    
+
 ]

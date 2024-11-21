1b65f93f708f1fb27cddaa6dc0ea49e5a39b5909
s84923 <you.sou1108@gmail.com>
2024-11-21 05:40:14 +0000

    a

--- test_list/urls.py
+++ test_list/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('test_list/', views.test_list, name='test_list'),
-    path('/test/test_edit/<int:student_id>/', views.test_edit, name='test_edit'),
+    path('test/test_edit/<int:student_id>/', views.test_edit, name='test_edit'),
 ]

4ec34a7de6b268b94f6abe657e211df28e7cd097
s84923 <you.sou1108@gmail.com>
2025-01-10 05:34:53 +0000

    a

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
-
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('studentmenu/', include('studentmenu.urls')),  # 生徒用メニュー
+    path('techermenu/', include('KDPS.urls')),  
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]

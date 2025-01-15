eb485aad5c38fd67d77cf97b916fdc3f92938f1d
s84923 <you.sou1108@gmail.com>
2025-01-15 01:17:57 +0000

    a

--- accounts/signals.py
+++ accounts/signals.py
@@ -11,4 +11,5 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     # Profileが存在する場合のみsave()を呼び出す
+    if hasattr(instance, 'profile'):
         instance.profile.save()

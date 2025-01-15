04a2e002922bf1700cae393ae2dd3dee12b93aa6
root <oky2384893@stu.o-hara.ac.jp>
2025-01-15 06:14:52 +0000

    gahaha

--- test_list/views.py
+++ test_list/views.py
@@ -32,28 +32,23 @@ def new_tests(request):
 
     return render(request, 'new_tests.html', {'form': form})
 
-# 削除後にメッセージを表示
-def delete_test(request, test_id):
-    test = get_object_or_404(Test, test_id=test_id)
-    if request.method == 'POST':
-        test.delete()
-        messages.success(request, '試験が削除されました。')  # メッセージを送信
-        return redirect('test_list')
-    return HttpResponse(status=405)
-
 # 試験編集ビュー
 def test_edit(request, test_id):
     # 試験データを取得
     instance = get_object_or_404(Test, test_id=test_id)
 
     if request.method == 'POST':
-        form = ExamEditForm(request.POST, instance=instance)
-        if form.is_valid():
-            form.save()
-            messages.success(request, '試験が更新されました。')  # メッセージを送信
-            return redirect('test_list')  # 編集後に試験一覧にリダイレクト
+        if 'delete' in request.POST:  # 削除処理
+            instance.delete()
+            messages.success(request, '試験が削除されました。')
+            return redirect('test_list')  # 削除後に試験一覧にリダイレクト
+        else:  # 編集処理
+            form = ExamEditForm(request.POST, instance=instance)
+            if form.is_valid():
+                form.save()
+                messages.success(request, '試験が更新されました。')  # メッセージを送信
+                return redirect('test_list')  # 編集後に試験一覧にリダイレクト
     else:
         form = ExamEditForm(instance=instance, initial={'test_id': instance.test_id})
 
     return render(request, 'test_list/test_edit.html', {'form': form})
-
